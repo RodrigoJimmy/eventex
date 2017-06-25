61e12f03a347955eea8061aab8250537cdad6d58
rodrigoSyscop <rodrigodelimavieira@gmail.com>
2017-06-25 10:25:50 -0300

    Extrai urls hardcoded

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.core import mail
-from django.http import HttpResponseRedirect, Http404
+from django.http import Http404, HttpResponseRedirect
+from django.shortcuts import resolve_url as r
 from django.shortcuts import render
 from django.template.loader import render_to_string
 
@@ -8,41 +9,44 @@ from eventex.subscriptions.forms import SubscriptionForm
 from eventex.subscriptions.models import Subscription
 
 
-def subscribe(request):
+def new(request):
     if request.method == 'POST':
         return create(request)
-    else:
-        return new(request)
+    
+    return empty_form(request)
+
+
+def empty_form(request):
+    return render(request, 'subscriptions/subscription_form.html',
+                  {'form': SubscriptionForm()})
+
 
 def create(request):
     form = SubscriptionForm(request.POST)
     if not form.is_valid():
         return render(request, 'subscriptions/subscription_form.html',
-            {'form': form})
+                      {'form': form})
 
     subscription = Subscription.objects.create(**form.cleaned_data)
-    
+
     _send_mail('Confirmação de inscrição',
-        settings.DEFAULT_FROM_EMAIL,
-        subscription.email,
-        'subscriptions/subscription_email.txt',
-        {'subscription': subscription})
+               settings.DEFAULT_FROM_EMAIL,
+               subscription.email,
+               'subscriptions/subscription_email.txt',
+               {'subscription': subscription})
 
-    return HttpResponseRedirect('/inscricao/{}/'.format(subscription.pk))
+    return HttpResponseRedirect(r('subscriptions:detail', subscription.pk))
 
-
-def new(request):
-    return render(request, 'subscriptions/subscription_form.html',
-        {'form': SubscriptionForm()})
 
 def detail(request, pk):
     try:
         subscription = Subscription.objects.get(pk=pk)
     except Subscription.DoesNotExist:
         raise Http404
-    
+
     return render(request, 'subscriptions/subscription_detail.html',
                   {'subscription': subscription})
+
 
 def _send_mail(subject, from_, to, template_name, context):
     body = render_to_string(template_name, context)
