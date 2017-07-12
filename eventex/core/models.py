9c6d932f398024568a0187518cf5d8bafa646c8e
Rodrigo Vieira <rodrigodelimavieira@gmail.com>
2017-07-12 17:08:31 -0300

    Contatos

--- eventex/core/models.py
+++ eventex/core/models.py
@@ -3,6 +3,7 @@ from django.shortcuts import resolve_url as r
 
 # Create your models here.
 class Speaker(models.Model):
+    objects = models.Manager()
     name = models.CharField('nome', max_length=255)
     slug = models.SlugField('slug')
     photo = models.URLField('foto')
@@ -18,3 +19,24 @@ class Speaker(models.Model):
 
     def get_absolute_url(self):
         return r('speaker_detail', slug=self.slug)
+
+
+class Contact(models.Model):
+    EMAIL = 'E'
+    PHONE = 'P'
+    KINDS = (
+        (EMAIL, 'Email'),
+        (PHONE, 'Telefone'),
+    )
+
+    objects = models.Manager()
+    speaker = models.ForeignKey('Speaker', verbose_name='palestrante')
+    kind = models.CharField('tipo', max_length=1, choices=KINDS)
+    value = models.CharField('valor', max_length=255)
+
+    class Meta:
+        verbose_name = 'contato'
+        verbose_name_plural = 'contatos'
+
+    def __str__(self):
+        return self.value
