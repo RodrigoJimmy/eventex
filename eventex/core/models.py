d1e30743f2babe119b5358cd766157e6fc97f46b
Rodrigo Vieira <rodrigodelimavieira@gmail.com>
2017-07-13 11:01:17 -0300

    Managers

--- eventex/core/models.py
+++ eventex/core/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 from django.shortcuts import resolve_url as r
 
+from eventex.core.managers import KindQuerySet, PeriodManager
+
+
 # Create your models here.
 class Speaker(models.Model):
-    objects = models.Manager()
     name = models.CharField('nome', max_length=255)
     slug = models.SlugField('slug')
     photo = models.URLField('foto')
     website = models.URLField('website', blank=True)
     description = models.TextField('descrição', blank=True)
+
+    objects = models.Manager()
 
     class Meta:
         verbose_name = 'palestrante'
@@ -29,10 +33,12 @@ class Contact(models.Model):
         (PHONE, 'Telefone'),
     )
 
-    objects = models.Manager()
+    
     speaker = models.ForeignKey('Speaker', verbose_name='palestrante')
     kind = models.CharField('tipo', max_length=1, choices=KINDS)
     value = models.CharField('valor', max_length=255)
+
+    objects = KindQuerySet.as_manager()    
 
     class Meta:
         verbose_name = 'contato'
@@ -43,11 +49,13 @@ class Contact(models.Model):
 
 
 class Talk(models.Model):
-    objects = models.Manager()
+    
     title = models.CharField('título', max_length=200)
     start = models.TimeField('início', blank=True, null=True)
     description = models.TextField('descrição', blank=True)
     speakers = models.ManyToManyField('Speaker', verbose_name='palestrantes', blank=True)
+
+    objects = PeriodManager()
 
     class Meta:
         verbose_name = 'palestra'
